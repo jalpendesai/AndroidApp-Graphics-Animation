adea430f672bbbb1406ec686fd4e493f55c8a937
jalpendesai <djalpen24@gmail.com>
2018-10-08 22:54:19 -0400

    Added AlertDialog

--- GraphicsAndAnimation/app/src/main/java/com/example/jdyeah/jalpendesai_comp304_ass3/MainActivity.java
+++ GraphicsAndAnimation/app/src/main/java/com/example/jdyeah/jalpendesai_comp304_ass3/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jdyeah.jalpendesai_comp304_ass3;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.dialog_name).setTitle(R.string.dialog_title);
+
+
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(),
+                        "YESsssszzz", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(),
+                        "NOoooo...", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
+
+
+
 }
